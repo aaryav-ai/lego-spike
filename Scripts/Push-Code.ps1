796fcb47240379354a9ef4ed443358db074f52a2
Aaryav Taparia <aaryav.taparia@gmail.com>
2024-11-03 17:14:41 -0800

    11/3/2024 5:14:41 PM - code from Aaryav's laptop

---
+++ Scripts/Push-Code.ps1
@@ -0,0 +1,6 @@
+cd C:\Users\aarya\code\lego-spike
+git pull
+.\Scripts\GenerateDescriptions.ps1 -type Missions -username aaryav
+git add .
+$m = (Get-Date).ToString() + " - code from Aaryav's laptop"
+git commit -m $m
